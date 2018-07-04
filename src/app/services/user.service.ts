0952d38b56f1a327a1f57dea8dac420c712a31cb
navishgitlab <navish@mobilyte.com>
2018-07-04 17:08:30 +0530

    second commit

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {User} from '../models/user';
+import { User } from '../models/user';
 
 @Injectable()
 export class UserService {
 
   constructor(private http: HttpClient) { }
 
-  create(user: User) {
-    return this.http.post('/api/users', user);
+  getById(id: number) {
+    return this.http.get(`/users/` + id);
+  }
+
+  register(user: User) {
+    return this.http.post(`/users/register`, user);
+  }
+
+  update(user: User) {
+    return this.http.put(`/users/` + user._id, user);
   }
 }
